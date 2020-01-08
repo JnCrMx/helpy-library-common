5ca1812d9658fe49a40bf638d0e9b536ae182735
JCM <jacama2009@gmail.com>
2020-01-08 16:51:03 +0100

    better toString for GeoLocation

--- src/main/java/de/jcm/helpy/GeoLocation.java
+++ src/main/java/de/jcm/helpy/GeoLocation.java
@@ -4,4 +4,22 @@ public class GeoLocation
 {
 	public double latitude;
 	public double longitude;
+
+	private String toDegreeString(double d)
+	{
+		double degrees = Math.floor(d);
+		double minutes = Math.floor(60.0*(d-degrees));
+		double seconds = 3600.0 * (d-degrees) - 60 * minutes;
+
+		return degrees +"° "+minutes+"' "+seconds+"''";
+	}
+
+	@Override
+	public String toString()
+	{
+		char lat = latitude<0?'S':'N';
+		char lon = latitude<0?'E':'W';
+
+		return toDegreeString(latitude)+" "+lat+", "+toDegreeString(longitude)+" "+lon;
+	}
 }
